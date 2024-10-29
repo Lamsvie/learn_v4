c265601576249c0f74f8781605434ebf22525f94
Lamsvie <dmohamedlamarana@gmail.com>
2024-10-29 11:36:46 +0000

    Reactstate_tp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [person, setPerson] = useState({
+    fullname: "Mohamed DIALLO",
+    bio: "developpeur junior ",
+    imgscr: "./img/thing",
+    profession: "Employe/Etudiant"
+  });
+  
+  const [time, setTime] = useState(0)
+
+  const [show, setshow] = useState(false);
+  
+  const interval = (start) => {
+    
+    const end = new Date()
+    console.log(end.getMilliseconds());
+    
+
+    const intervale = end.getMilliseconds() - start.getMilliseconds()
+
+    setTime(intervale)
+    
+  }
+
+  const intervaleState = () => {
+    const start = new Date()
+    console.log(start.getMilliseconds());
+    
+    setshow(!show)
+    
+    interval(start)
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={intervaleState} className='bg-blue-300 p-2 font-semibold'>Show</button>
+      { show ? (<h1 className=" font-bold">
+        {person.fullname} <br/>
+        {person.bio} <br/>
+        {person.imgscr} <br/>
+        {person.profession}
+    </h1>): "" }
+    <input value={time} className='h-10' />
     </div>
   );
 }
